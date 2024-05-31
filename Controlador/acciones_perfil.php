aeffbe78581ca2f07c035c09a07a5f633c3dbc7c
AymanSbayy <a.sbay@sapalomera.cat>
2024-05-31 23:13:22 +0200

    Readme finalizado y algunos comentarios

--- Controlador/acciones_perfil.php
+++ Controlador/acciones_perfil.php
@@ -1,40 +1,55 @@
 <?php
 
+// Se incluyen los archivos necesarios
 require_once("../Model/consultas_usuarios.php");
 require_once("../Middleware/LoggedIn.php");
 require_once("validations.php");
 
+// Se verifica si la solicitud es de tipo POST
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    // Se verifica si la acción es cambiar contraseña
     if (isset($_POST['action']) && $_POST['action'] === 'change_password') {
+        // Se obtienen las contraseñas actuales y nuevas
         $current_password = $_POST['currentPassword'];
         $new_password = $_POST['newPassword'];
 
+        // Se verifica que no haya campos vacíos
         if (empty($current_password) || empty($new_password)) {
             echo json_encode(array('status' => 'error', 'message' => 'Por favor, rellena todos los campos'));
             return;
         }
 
+        // Se valida la nueva contraseña
         if (validatePassword($new_password)) {
             echo json_encode(array('status' => 'error', 'message' => 'La contraseña debe tener al menos 8 caracteres, una letra mayúscula, una letra minúscula y un número'));
             return;
         }
 
+        // Se obtiene el usuario actual
         $user = getUserByEmail($_SESSION["usuario"]);
 
+        // Se verifica si el usuario ha iniciado sesión con un proveedor externo
         if ($user['provider'] !== null) {
             echo json_encode(array('status' => 'error', 'message' => 'No puedes cambiar la contraseña si has iniciado sesión con login social'));
             return;
         }
 
+        // Se verifica si la contraseña actual es correcta
         if (password_verify($current_password, $user['password'])) {
+            // Se encripta la nueva contraseña
             $new_password = password_hash($new_password, PASSWORD_DEFAULT);
+            // Se cambia la contraseña en la base de datos
             cambiarPassword($new_password, $user['idUsuario']);
             echo json_encode(array('status' => 'success', 'message' => 'Contraseña cambiada correctamente'));
         } else {
             echo json_encode(array('status' => 'error', 'message' => 'La contraseña que has introducido no es correcta'));
         }
-    } else if (isset($_POST['action']) && $_POST['action'] === 'change_personal_info') {
+    } 
+    // Se verifica si la acción es cambiar información personal
+    else if (isset($_POST['action']) && $_POST['action'] === 'change_personal_info') {
+        // Se verifica si se ha enviado una foto
         if (isset($_FILES['foto'])) {
+            // Se verifica el tamaño de la foto
             if ($_FILES['foto']['size'] > 5000000) {
                 echo json_encode(array('status' => 'error', 'message' => 'La imagen no puede pesar más de 5MB'));
                 return;
@@ -44,6 +59,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             $foto = "";
         }
 
+        // Se obtienen los datos de nombre y teléfono
         if (isset($_POST['nombre'])) {
             $nombre = $_POST['nombre'];
         } else {
@@ -56,19 +72,23 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             $telefono = "";
         }
 
+        // Se verifica que al menos un campo esté lleno
         if (empty($nombre) && empty($telefono) && empty($foto['name'])) {
             echo json_encode(array('status' => 'error', 'message' => 'Por favor, rellena al menos un campo'));
             return;
         }
 
+        // Se obtiene el usuario actual
         $user = getUserByEmail($_SESSION["usuario"]);
 
+        // Se verifica si el nombre está vacío
         if (empty($nombre)) {
             $nombre = $user['nombre'];
         } else if (validateNombreCompleto($nombre)) {
             echo json_encode(array('status' => 'error', 'message' => 'El nombre y apellidos no pueden tener más de 100 caracteres'));
             return;
         }
+        // Se verifica si el teléfono está vacío
         if (empty($telefono)) {
             $telefono = $user['telefono'];
         } else if (validateTelefono($telefono)) {
@@ -76,21 +96,28 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             return;
         }
 
+        // Se verifica si la foto está vacía
         if (empty($foto['name'])) {
             $foto = $user['foto'];
         } else {
+            // Se obtiene el contenido de la foto y se codifica en base64
             $foto = file_get_contents($foto['tmp_name']);
             $foto = base64_encode($foto);
         }
 
+        // Se actualizan los datos personales en la base de datos
         cambiarDatosPersonales($nombre, $telefono, $foto, $user['idUsuario']);
         echo json_encode(array('status' => 'success', 'message' => 'Datos personales cambiados correctamente'));
-    } else if (isset($_POST['action']) && $_POST['action'] === 'save_shipping_info') {
+    } 
+    // Se verifica si la acción es guardar información de envío
+    else if (isset($_POST['action']) && $_POST['action'] === 'save_shipping_info') {
         $id = "";
         if (isset($_POST['id'])) {
             $id = $_POST['id'];
         }
+        // Se verifica si el ID está vacío
         if (empty($id)) {
+            // Se verifica que no haya campos vacíos
             if (!empty($_POST['nombre']) && !empty($_POST['direccion']) && !empty($_POST['ciudad']) && !empty($_POST['provincia']) && !empty($_POST['codigoPostal']) && !empty($_POST['pais'])) {
                 $nombre = $_POST['nombre'];
                 $direccion = $_POST['direccion'];
@@ -99,7 +126,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
                 $codigoPostal = $_POST['codigoPostal'];
                 $pais = $_POST['pais'];
 
-
+                // Se validan los campos de dirección, ciudad, provincia, código postal y país
                 if (validateDireccion($direccion)) {
                     echo json_encode(array('status' => 'error', 'message' => 'La dirección que has introducido no es válida'));
                     return;
@@ -125,14 +152,17 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
                     return;
                 }
 
+                // Se obtiene el usuario actual
                 $user = getUserByEmail($_SESSION["usuario"]);
 
+                // Se añade la dirección de envío en la base de datos
                 añadirDireccion($nombre, $direccion, $ciudad, $provincia, $codigoPostal, $pais, $user['idUsuario']);
                 echo json_encode(array('status' => 'success', 'message' => 'Dirección añadida correctamente'));
             } else {
                 echo json_encode(array('status' => 'error', 'message' => 'Por favor, rellena todos los campos'));
             }
         } else {
+            // Se verifica que no haya campos vacíos
             if (!empty($_POST['nombre']) && !empty($_POST['direccion']) && !empty($_POST['ciudad']) && !empty($_POST['provincia']) && !empty($_POST['codigoPostal']) && !empty($_POST['pais'])) {
                 $nombre = $_POST['nombre'];
                 $direccion = $_POST['direccion'];
@@ -142,7 +172,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
                 $pais = $_POST['pais'];
                 $id = $_POST['id'];
 
-
+                // Se validan los campos de dirección, ciudad, provincia, código postal y país
                 if (validateDireccion($direccion)) {
                     echo json_encode(array('status' => 'error', 'message' => 'La dirección que has introducido no es válida'));
                     return;
@@ -168,20 +198,26 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
                     return;
                 }
 
+                // Se obtiene el usuario actual
                 $user = getUserByEmail($_SESSION["usuario"]);
 
+                // Se actualiza la dirección de envío en la base de datos
                 actualizarDireccion($id, $nombre, $direccion, $ciudad, $provincia, $codigoPostal, $pais, $user['idUsuario']);
                 echo json_encode(array('status' => 'success', 'message' => 'Dirección actualizada correctamente'));
             } else {
                 echo json_encode(array('status' => 'error', 'message' => 'Por favor, rellena todos los campos'));
             }
         }
-    } else if (isset($_POST['action']) && $_POST['action'] === 'save_billing_info') {
+    } 
+    // Se verifica si la acción es guardar información de facturación
+    else if (isset($_POST['action']) && $_POST['action'] === 'save_billing_info') {
         $id = "";
         if (isset($_POST['id'])) {
             $id = $_POST['id'];
         }
+        // Se verifica si el ID está vacío
         if (empty($id)) {
+            // Se verifica que no haya campos vacíos
             if (!empty($_POST['nombre']) && !empty($_POST['direccion']) && !empty($_POST['ciudad']) && !empty($_POST['provincia']) && !empty($_POST['codigoPostal']) && !empty($_POST['pais'])) {
                 $nombre = $_POST['nombre'];
                 $direccion = $_POST['direccion'];
@@ -190,6 +226,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
                 $codigoPostal = $_POST['codigoPostal'];
                 $pais = $_POST['pais'];
 
+                // Se validan los campos de dirección, ciudad, provincia, código postal y país
                 if (validateDireccion($direccion)) {
                     echo json_encode(array('status' => 'error', 'message' => 'La dirección que has introducido no es válida'));
                     return;
@@ -215,14 +252,17 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
                     return;
                 }
 
+                // Se obtiene el usuario actual
                 $user = getUserByEmail($_SESSION["usuario"]);
 
+                // Se añaden los datos de facturación en la base de datos
                 añadirDatosFacturacion($nombre, $direccion, $ciudad, $provincia, $codigoPostal, $pais, $user['idUsuario']);
                 echo json_encode(array('status' => 'success', 'message' => 'Datos de facturación añadidos correctamente', 'id' => $id, 'direccion' => $direccion));
             } else {
                 echo json_encode(array('status' => 'error', 'message' => 'Por favor, rellena todos los campos'));
             }
         } else {
+            // Se verifica que no haya campos vacíos
             if (!empty($_POST['nombre']) && !empty($_POST['direccion']) && !empty($_POST['ciudad']) && !empty($_POST['provincia']) && !empty($_POST['codigoPostal']) && !empty($_POST['pais'])) {
                 $nombre = $_POST['nombre'];
                 $direccion = $_POST['direccion'];
@@ -232,6 +272,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
                 $pais = $_POST['pais'];
                 $id = $_POST['id'];
 
+                // Se validan los campos de dirección, ciudad, provincia, código postal y país
                 if (validateDireccion($direccion)) {
                     echo json_encode(array('status' => 'error', 'message' => 'La dirección que has introducido no es válida'));
                     return;
@@ -257,8 +298,10 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
                     return;
                 }
 
+                // Se obtiene el usuario actual
                 $user = getUserByEmail($_SESSION["usuario"]);
 
+                // Se actualizan los datos de facturación en la base de datos
                 actualizarDatosFacturacion($id, $nombre, $direccion, $ciudad, $provincia, $codigoPostal, $pais, $user['idUsuario']);
                 echo json_encode(array('status' => 'success', 'message' => 'Datos de facturación actualizados correctamente'));
             } else {
--- Controlador/acciones_usuarios.php
+++ Controlador/acciones_usuarios.php
@@ -3,10 +3,14 @@
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     require_once("../Model/consultas_categorias.php");
 
+    // Verificar si se ha enviado el parámetro 'categoria_id'
     if (isset($_POST['categoria_id'])) {
         $categoriaId = $_POST['categoria_id'];
+        // Obtener las subcategorías según la categoría proporcionada
         $subcategorias = getSubcategoriesByCategory($categoriaId);
+        // Establecer el encabezado de respuesta como JSON
         header('Content-Type: application/json');
+        // Devolver las subcategorías en formato JSON
         echo json_encode($subcategorias);
     }
 }
@@ -16,11 +20,13 @@ if ($_SERVER['REQUEST_METHOD'] === 'DELETE') {
     require_once("../Middleware/isAdmin.php");
     require_once("../Model/consultas_usuarios.php");
 
+    // Obtener los parámetros 'id' y 'accion' de la URL
     $idUsuario = $_GET['id'];
     $accion = $_GET['accion'];
 
     if ($accion === 'eliminar') {
         try {
+            // Eliminar el usuario según el ID proporcionado
             deleteUser($idUsuario);
             echo json_encode(['message' => 'Usuario eliminado']);
         } catch (Exception $e) {
@@ -34,11 +40,13 @@ if ($_SERVER['REQUEST_METHOD'] === 'PUT') {
     require_once("../Middleware/isAdmin.php");
     require_once("../Model/consultas_usuarios.php");
 
+    // Obtener los parámetros 'id' y 'accion' de la URL
     $idUsuario = $_GET['id'];
     $accion = $_GET['accion'];
 
     if ($accion === 'bloquear') {
         try {
+            // Bloquear al usuario según el ID proporcionado
             bloquearUser($idUsuario);
             echo json_encode(['message' => 'Usuario bloqueado']);
         } catch (Exception $e) {
@@ -46,6 +54,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'PUT') {
         }
     } elseif ($accion === 'desbloquear') {
         try {
+            // Desbloquear al usuario según el ID proporcionado
             desbloquearUser($idUsuario);
             echo json_encode(['message' => 'Usuario desbloqueado']);
         } catch (Exception $e) {
@@ -53,6 +62,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'PUT') {
         }
     } elseif ($accion === 'quitar-admin') {
         try {
+            // Quitar el rol de administrador al usuario según el ID proporcionado
             quitarAdmin($idUsuario);
             echo json_encode(['message' => 'Usuario quitado de administrador']);
         } catch (Exception $e) {
@@ -60,6 +70,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'PUT') {
         }
     } elseif ($accion === 'hacer-admin') {
         try {
+            // Asignar el rol de administrador al usuario según el ID proporcionado
             hacerAdmin($idUsuario);
             echo json_encode(['message' => 'Usuario hecho administrador']);
         } catch (Exception $e) {
